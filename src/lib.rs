077c1952cfc970d80e6763c78c29247f31893c7b
Glenn McAllsiter <glenn.mcallister@gmail.com>
2015-06-26 21:52:11 -0400

    Add dependency to serde crate
    I'm going to need to serialize and deserialize JSON, so I've picked serde to do
    it with. I've added a dummy unit test just to exercise the deserialization code
    a bit to see how it works.

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,29 @@
-
+extern crate serde;
 
 // Pure unit tests
 #[cfg(test)]
 mod tests {
-    // use super::*;
+    //use super::*;
+    use serde::json::{self, Value};
+
 
     #[test]
-    fn it_works() {
+    fn extercise_json() {
+        // This is basically a cut-n-paste of the serde json example code, but
+        // changing the println! statements into assertions. It's here just so
+        // I can get some practice with the JSON deserialization code.
+
+        let data: Value = json::from_str("{\"foo\": 13, \"bar\": \"baz\"}").unwrap();
+        assert!(data.is_object());
+
+        let obj = data.as_object().unwrap();
+        let foo = obj.get("foo").unwrap();
+
+        assert_eq!(foo.as_array(), None);
+        assert_eq!(foo.is_number(), true);
+        assert_eq!(foo.is_u64(), true);     // 13 can be coerced into a u64
+        assert_eq!(foo.as_u64().unwrap(), 13);
+        assert_eq!(foo.is_null(), false);
+        assert_eq!(foo.is_f64(), false);
     }
 }
