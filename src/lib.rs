5b0aff995d18fa2cc792adcf2ce8685f4773a8ca
Glenn McAllsiter <glenn.mcallister@gmail.com>
2015-06-27 00:38:14 -0400

    Figure out how to use serde_macro and friends correctly
    After a lot of messing around, I managed to figure out how to use Serde 0.4.0
    correctly. I *really* wish I'd found
    http://erickt.github.io/blog/2015/05/18/serde-0-dot-4-0-now-supports-macros-in-stable-rust/
    earlier, as it actually has the process documented, where the GitHub README.md
    does not.
    
    At the moment, all this really has it a bogus Point struct which I can
    serialize to a string and deserialize from a string as JSON, and prove to my
    satisfaction that it's valid both ways. Next I have to dig into the Avro schema
    spec and turn that into a viable object model I can manipulate with Serde.

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,34 @@
+#![cfg_attr(feature = "nightly", feature(plugin))]
+#![cfg_attr(feature = "nightly", plugin(serde_macros))]
+
 extern crate serde;
+
+use std::cmp::PartialEq;
+
+#[cfg(feature = "nightly")]
+include!("lib.rs.in");
+
+#[cfg(feature = "with_syntex")]
+include!(concat!(env!("OUT_DIR"), "/lib.rs"));
+
+impl PartialEq<Point> for Point {
+    fn eq(&self, other: &Point) -> bool {
+        self.x == other.x && self.y == other.y
+    }
+
+    fn ne(&self, other: &Point) -> bool {
+        !self.eq(&other)
+    } 
+}
 
 // Pure unit tests
 #[cfg(test)]
 mod tests {
-    //use super::*;
+    use super::*;
     use serde::json::{self, Value};
 
-
     #[test]
-    fn extercise_json() {
+    fn exercise_json() {
         // This is basically a cut-n-paste of the serde json example code, but
         // changing the println! statements into assertions. It's here just so
         // I can get some practice with the JSON deserialization code.
@@ -25,5 +45,17 @@ mod tests {
         assert_eq!(foo.as_u64().unwrap(), 13);
         assert_eq!(foo.is_null(), false);
         assert_eq!(foo.is_f64(), false);
+    }
+
+    #[test]
+    fn exercise_point() {
+        let point = Point { x: 1, y: 2 };
+        let ser_point = json::to_string(&point).unwrap();
+
+        assert_eq!(ser_point, "{\"x\":1,\"y\":2}");
+
+        let des_point : Point = json::from_str(&ser_point).unwrap();
+
+        assert_eq!(point, des_point);
     }
 }
