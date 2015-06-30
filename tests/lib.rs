aa57f4ddc87d50f91ead491e999acb579caa79b4
Glenn McAllsiter <glenn.mcallister@gmail.com>
2015-06-29 21:40:58 -0400

    Start adding support for "record" type
    This is going to take a while to make sure I get all the permutations right,
    but I can deserialize a very, very basic record JSON string with an empty field
    array and serialize it back into our overly verbose JSON.

--- tests/lib.rs
+++ tests/lib.rs
@@ -12,7 +12,7 @@ fn ser_null_type() {
     let null_type = schema::from_str(&"null").unwrap();
     let ser_null_type = json::to_string(&null_type).unwrap();
 
-    assert_eq!(ser_null_type, r#"{"type":"null","namespace":null,"doc":null,"aliases":[],"fields":[]}"#);
+    assert_eq!(ser_null_type, r#"{"type":"null","name":null,"namespace":null,"doc":null,"aliases":[],"fields":[]}"#);
 }
 
 #[test]
@@ -33,7 +33,7 @@ fn ser_boolean_type() {
     let bool_type = schema::from_str(&"boolean").unwrap();
     let ser_bool_type = json::to_string(&bool_type).unwrap();
 
-    assert_eq!(ser_bool_type, r#"{"type":"boolean","namespace":null,"doc":null,"aliases":[],"fields":[]}"#);
+    assert_eq!(ser_bool_type, r#"{"type":"boolean","name":null,"namespace":null,"doc":null,"aliases":[],"fields":[]}"#);
 }
 
 #[test]
@@ -54,7 +54,7 @@ fn ser_int_type() {
     let int_type = schema::from_str(&"int").unwrap();
     let ser_int_type = json::to_string(&int_type).unwrap();
 
-    assert_eq!(ser_int_type, r#"{"type":"int","namespace":null,"doc":null,"aliases":[],"fields":[]}"#);
+    assert_eq!(ser_int_type, r#"{"type":"int","name":null,"namespace":null,"doc":null,"aliases":[],"fields":[]}"#);
 }
 
 #[test]
@@ -75,7 +75,7 @@ fn ser_long_type() {
     let long_type = schema::from_str(&"long").unwrap();
     let ser_long_type = json::to_string(&long_type).unwrap();
 
-    assert_eq!(ser_long_type, r#"{"type":"long","namespace":null,"doc":null,"aliases":[],"fields":[]}"#);
+    assert_eq!(ser_long_type, r#"{"type":"long","name":null,"namespace":null,"doc":null,"aliases":[],"fields":[]}"#);
 }
 
 #[test]
@@ -96,7 +96,7 @@ fn ser_float_type() {
     let float_type = schema::from_str(&"float").unwrap();
     let ser_float_type = json::to_string(&float_type).unwrap();
 
-    assert_eq!(ser_float_type, r#"{"type":"float","namespace":null,"doc":null,"aliases":[],"fields":[]}"#);
+    assert_eq!(ser_float_type, r#"{"type":"float","name":null,"namespace":null,"doc":null,"aliases":[],"fields":[]}"#);
 }
 
 #[test]
@@ -117,7 +117,7 @@ fn ser_double_type() {
     let double_type = schema::from_str(&"double").unwrap();
     let ser_double_type = json::to_string(&double_type).unwrap();
 
-    assert_eq!(ser_double_type, r#"{"type":"double","namespace":null,"doc":null,"aliases":[],"fields":[]}"#);
+    assert_eq!(ser_double_type, r#"{"type":"double","name":null,"namespace":null,"doc":null,"aliases":[],"fields":[]}"#);
 }
 
 #[test]
@@ -138,7 +138,7 @@ fn ser_bytes_type() {
     let bytes_type = schema::from_str(&"bytes").unwrap();
     let ser_bytes_type = json::to_string(&bytes_type).unwrap();
 
-    assert_eq!(ser_bytes_type, r#"{"type":"bytes","namespace":null,"doc":null,"aliases":[],"fields":[]}"#);
+    assert_eq!(ser_bytes_type, r#"{"type":"bytes","name":null,"namespace":null,"doc":null,"aliases":[],"fields":[]}"#);
 }
 
 #[test]
@@ -159,7 +159,7 @@ fn ser_string_type() {
     let string_type = schema::from_str(&"string").unwrap();
     let ser_string_type = json::to_string(&string_type).unwrap();
 
-    assert_eq!(ser_string_type, r#"{"type":"string","namespace":null,"doc":null,"aliases":[],"fields":[]}"#);
+    assert_eq!(ser_string_type, r#"{"type":"string","name":null,"namespace":null,"doc":null,"aliases":[],"fields":[]}"#);
 }
 
 #[test]
@@ -185,39 +185,48 @@ fn des_bogus_type() {
 }
 
 #[test]
+fn ser_record_type_1() {
+    // This is about the simplest record serialization test I can think of
+    let rec_type = schema::from_str(&r#"{"type":"record", "name":"foo", "fields":[]}"#).unwrap();
+    let ser_rec_type = json::to_string(&rec_type).unwrap();
+
+    assert_eq!(ser_rec_type, r#"{"type":"record","name":"foo","namespace":null,"doc":null,"aliases":[],"fields":[]}"#)
+}
+
+#[test]
 fn check_raw_simple_type_conversions() {
     let null_raw = "null".to_string();
-    assert_eq!(null_raw, PrimitiveTypeEnum::Null.to_raw());
-    assert_eq!(PrimitiveTypeEnum::Null, PrimitiveTypeEnum::from_raw(&null_raw).unwrap());
+    assert_eq!(null_raw, PrimitiveTypeEnum::Null.to_string());
+    assert_eq!(PrimitiveTypeEnum::Null, PrimitiveTypeEnum::from_string(&null_raw).unwrap());
 
     let boolean_raw = "boolean".to_string();
-    assert_eq!(boolean_raw, PrimitiveTypeEnum::Boolean.to_raw());
-    assert_eq!(PrimitiveTypeEnum::Boolean, PrimitiveTypeEnum::from_raw(&boolean_raw).unwrap());
+    assert_eq!(boolean_raw, PrimitiveTypeEnum::Boolean.to_string());
+    assert_eq!(PrimitiveTypeEnum::Boolean, PrimitiveTypeEnum::from_string(&boolean_raw).unwrap());
 
     let int_raw = "int".to_string();
-    assert_eq!(int_raw, PrimitiveTypeEnum::Int.to_raw());
-    assert_eq!(PrimitiveTypeEnum::Int, PrimitiveTypeEnum::from_raw(&int_raw).unwrap());
+    assert_eq!(int_raw, PrimitiveTypeEnum::Int.to_string());
+    assert_eq!(PrimitiveTypeEnum::Int, PrimitiveTypeEnum::from_string(&int_raw).unwrap());
 
     let long_raw = "long".to_string();
-    assert_eq!(long_raw, PrimitiveTypeEnum::Long.to_raw());
-    assert_eq!(PrimitiveTypeEnum::Long, PrimitiveTypeEnum::from_raw(&long_raw).unwrap());
+    assert_eq!(long_raw, PrimitiveTypeEnum::Long.to_string());
+    assert_eq!(PrimitiveTypeEnum::Long, PrimitiveTypeEnum::from_string(&long_raw).unwrap());
 
     let float_raw = "float".to_string();
-    assert_eq!(float_raw, PrimitiveTypeEnum::Float.to_raw());
-    assert_eq!(PrimitiveTypeEnum::Float, PrimitiveTypeEnum::from_raw(&float_raw).unwrap());
+    assert_eq!(float_raw, PrimitiveTypeEnum::Float.to_string());
+    assert_eq!(PrimitiveTypeEnum::Float, PrimitiveTypeEnum::from_string(&float_raw).unwrap());
 
     let double_raw = "double".to_string();
-    assert_eq!(double_raw, PrimitiveTypeEnum::Double.to_raw());
-    assert_eq!(PrimitiveTypeEnum::Double, PrimitiveTypeEnum::from_raw(&double_raw).unwrap());
+    assert_eq!(double_raw, PrimitiveTypeEnum::Double.to_string());
+    assert_eq!(PrimitiveTypeEnum::Double, PrimitiveTypeEnum::from_string(&double_raw).unwrap());
 
     let bytes_raw = "bytes".to_string();
-    assert_eq!(bytes_raw, PrimitiveTypeEnum::Bytes.to_raw());
-    assert_eq!(PrimitiveTypeEnum::Bytes, PrimitiveTypeEnum::from_raw(&bytes_raw).unwrap());
+    assert_eq!(bytes_raw, PrimitiveTypeEnum::Bytes.to_string());
+    assert_eq!(PrimitiveTypeEnum::Bytes, PrimitiveTypeEnum::from_string(&bytes_raw).unwrap());
 
     let string_raw = "string".to_string();
-    assert_eq!(string_raw, PrimitiveTypeEnum::String.to_raw());
-    assert_eq!(PrimitiveTypeEnum::String, PrimitiveTypeEnum::from_raw(&string_raw).unwrap());
+    assert_eq!(string_raw, PrimitiveTypeEnum::String.to_string());
+    assert_eq!(PrimitiveTypeEnum::String, PrimitiveTypeEnum::from_string(&string_raw).unwrap());
 
     let bogus_raw = "bogus".to_string();
-    assert_eq!(None, PrimitiveTypeEnum::from_raw(&bogus_raw));
+    assert_eq!(None, PrimitiveTypeEnum::from_string(&bogus_raw));
 }
